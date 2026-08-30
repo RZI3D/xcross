9b5aa62e7f74da1b3ae8b6870bca02ea90741967
rzi3d <zackiesattaur@gmail.com>
2026-08-30 08:14:42 -0400

    fix(nix) patch swift binaries

--- flake.nix
+++ flake.nix
@@ -77,16 +77,32 @@
             src = pkgs.fetchurl {
               inherit (swiftToolchainSource) url hash;
             };
+            nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+            buildInputs = [
+              pkgs.stdenv.cc.cc.lib
+              pkgs.zlib
+              pkgs.ncurses
+              pkgs.libxml2_13
+              pkgs.libedit
+              pkgs.curl
+              pkgs.libuuid
+              pkgs.python312 # note: needs to be 3.12 specifically, matching libpython3.12.so.1.0
+              pkgs.sqlite
+            ];
 
             dontConfigure = true;
             dontBuild = true;
-            dontFixup = true;
+            autoPatchelfIgnoreMissingDeps = [ "libedit.so.2" ]; # TODO: find actual packages
 
             installPhase = ''
               runHook preInstall
               mkdir -p "$out"
               cp -r usr/* "$out/"
               runHook postInstall
+            '';
+            postFixup = ''
+              find $out -type f -executable -exec \
+                patchelf --replace-needed libedit.so.2 libedit.so.0 {} \; 2>/dev/null || true
             '';
           };
 
